f9e48c85900b1a0d79b552bd4a68f0e062ea6d5d
amateishchuk <amateishchuk@gmail.com>
2017-05-25 11:22:06 +0300

    Added all kinds of animals

---
+++ ZooApp/Abstract/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooApp.Enums;
+
+namespace ZooApp.Abstract
+{
+    public abstract class Animal
+    {
+        public string Name { get; protected set; }
+        public State State { get; protected set; }
+        public int Health { get; protected set; }
+        protected readonly int MAXHEALTH;
+
+        public Animal(string name, int maxhealth)
+        {
+            Name = name;
+            State = State.Full;
+            Health = maxhealth;
+            MAXHEALTH = maxhealth;
+        }
+
+    }
+}
---
+++ ZooApp/Concrete/Animals/Tiger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooApp.Abstract;
+
+namespace ZooApp.Concrete.Animals
+{
+    public class Tiger : Animal
+    {
+        public Tiger(string name) : base(name, 4)
+        {
+
+        }
+    }
+}
