5d40c0d58e5c5085c1b89eb633b0db3f8fc0b9b7
amateishchuk <amateishchuk@gmail.com>
2017-05-25 11:32:02 +0300

    Added all kinds of animals

---
+++ ZooApp/Concrete/AnimalFactories/ElephantFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooApp.Abstract;
+using ZooApp.Concrete.Animals;
+
+namespace ZooApp.Concrete.AnimalFactories
+{
+    public class ElephantFactory : IAnimalFactory
+    {
+        public Animal CreateAnimal(string name)
+        {
+            return new Elephant(name);
+        }
+    }
+}
---
+++ ZooApp/Concrete/AnimalFactories/WolfFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooApp.Abstract;
+using ZooApp.Concrete.Animals;
+
+namespace ZooApp.Concrete.AnimalFactories
+{
+    public class WolfFactory : IAnimalFactory
+    {
+        public Animal CreateAnimal(string name)
+        {
+            return new Wolf(name);
+        }
+    }
+}
