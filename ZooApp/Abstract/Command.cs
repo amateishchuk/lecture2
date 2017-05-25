2fe6242ef7f9621be3dd5059f2e39de53477e483
amateishchuk <amateishchuk@gmail.com>
2017-05-25 12:52:13 +0300

    Added commands

---
+++ ZooApp/Abstract/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp.Abstract
+{
+    public abstract class Command
+    {
+        protected IRepository repository;
+
+        public Command(IRepository repo)
+        {
+            repo = repository;
+        }
+        public abstract void Execute();
+    }
+}
---
+++ ZooApp/Concrete/Commands/InsertCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooApp.Abstract;
+
+namespace ZooApp.Concrete.Commands
+{
+    public class InsertCommand : Command
+    {
+        public InsertCommand(IRepository repo) : base(repo)
+        {
+        }
+
+        public override void Execute()
+        {
+            Console.Write("INSERTING: Input animal's name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("DELETING: Input animal's kind (lion, tiger, elephant, wolf, bear, fox): ");
+            string kind = Console.ReadLine();
+
+            repository.InsertAnimal(name, kind);
+        }
+    }
+}
